ef2a69d8bc98fb3ff418bcccc227f98986d7a418
Jake Harrison <jaketannerharrison@gmail.com>
2026-01-20 21:40:54 -0500

    implement authentication endpoint

--- webserver/src/api/db.php
+++ webserver/src/api/db.php
@@ -1,31 +1,22 @@
 <?php
-namespace Src\System;
 
-class DatabaseConnector {
+namespace Src\Api;
+use mysqli;
 
-    private $dbConnection = null;
-
-    public function __construct()
-    {
-        $host = getenv('DB_HOST');
-        $port = getenv('DB_PORT');
-        $db   = getenv('DB_DATABASE');
-        $user = getenv('DB_USERNAME');
-        $pass = getenv('DB_PASSWORD');
-
-        try {
-            $this->dbConnection = new \PDO(
-                "mysql:host=$host;port=$port;charset=utf8mb4;dbname=$db",
-                $user,
-                $pass
-            );
-        } catch (\PDOException $e) {
-            exit($e->getMessage());
-        }
+function dbConnect() 
+{
+    $host = getenv('DB_HOST');
+    $port = getenv('DB_PORT');
+    $database = getenv('DB_DATABASE');
+    $user = getenv('DB_USERNAME');
+    $pass = getenv('DB_PASSWORD');
+    
+    $connection = new mysqli($host, $user, $pass, $database, $port);
+    
+    if ($connection->connect_error) {
+        return null;
     }
-
-    public function getConnection()
-    {
-        return $this->dbConnection;
-    }
+    
+    return $connection;
 }
+
--- webserver/src/index.php
+++ webserver/src/index.php
@@ -3,5 +3,5 @@
 require_once __DIR__ . '/api/db.php';
 require_once __DIR__ . '/api/api.php';
 
-use Src\System\DatabaseConnector;
+
 
