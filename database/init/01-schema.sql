7935e6f14f7b7968075923acff59754d02656a08
Braxious <NevarezJosh2@gmail.com>
2026-01-22 19:02:58 -0500

    Added creation_date to schema

--- database/init/01-schema.sql
+++ database/init/01-schema.sql
@@ -16,7 +16,8 @@ CREATE TABLE contacts (
   first_name VARCHAR(100) NOT NULL,
   last_name VARCHAR(100) NOT NULL,
   email VARCHAR(255) NOT NULL,
-  phone VARCHAR(11),
+  phone VARCHAR(11) NOT NULL,
+  creation_date DATETIME DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id),
   FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE
 ) ENGINE = InnoDB
