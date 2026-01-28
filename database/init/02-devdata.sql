698a0f79fcf5943df402db416073389065e3d264
Jake Harrison <jaketannerharrison@gmail.com>
2026-01-28 17:42:44 -0500

    add email field to db schema

--- database/init/02-devdata.sql
+++ database/init/02-devdata.sql
@@ -1,9 +1,9 @@
 -- Populate schema with example data
 
-INSERT INTO users (username, password_hash) VALUES
-('alice', 'hashed_password_1'),
-('bob', 'hashed_password_2'),
-('charlie', 'hashed_password_3');
+INSERT INTO users (username, password_hash, email, token) VALUES
+('alice', 'hashed_password_1', 'alice@mail.com', 'placeholdertoken'),
+('bob', 'hashed_password_2', 'bob@mail.com', 'placeholdertoken2'),
+('charlie', 'hashed_password_3', 'charlie@mail.com', 'placeholdertoken3');
 
 INSERT INTO contacts (owner_id, first_name, last_name, email, phone) VALUES
 (1, 'John', 'Doe', 'jd@gmail.com', '1234567890'),
